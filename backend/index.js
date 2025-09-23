5449c63386ba645c77bdef9b80642883aa7cbba8
chaykrishna <2300031138@Kluniversity.in>
2025-09-23 07:56:43 +0530

    third commit

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
-
+const express = require('express');
 const mysql = require('mysql2');
+const cors = require('cors');
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// MySQL Connection
 const db = mysql.createConnection({
-  host: 'mysql',           // service name from docker-compose.yml
-  user: 'appuser',         // must match MYSQL_USER
-  password: 'Backend456!', // must match MYSQL_PASSWORD
-  database: 'grocery_db',  // must match MYSQL_DATABASE
+  host: 'mysql_db', // name of the mysql service from docker-compose
+  user: 'appuser',
+  password: 'Backend456!',
+  database: 'grocery_db',
 });
 
 db.connect((err) => {
@@ -14,4 +20,20 @@ db.connect((err) => {
   } else {
     console.log('Connected to MySQL!');
   }
+});
+
+// API to fetch products
+app.get('/api/products', (req, res) => {
+  db.query('SELECT * FROM products', (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Failed to fetch data' });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+app.listen(5000, () => {
+  console.log('Backend server running on port 5000');
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,31 @@
-import ProductList from "./pages/ProductList";
+import { useEffect } from "react";
+import AppDownload from "./components/AppDownload/AppDownload";
+import Footer from "./components/Footer/Footer";
+import Hero from "./components/Hero/Hero";
+import Services from "./components/Services/Services";
+import WhereToBuy from "./components/WhereToBuy/WhereToBuy";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-function App() {
+const App = () => {
+
+  useEffect(() => {
+    AOS.init({
+      // duration: 500,
+      // delay: 100,
+      // easing: "ease-in-cubic",
+    });
+  }, []);
+
   return (
-    <div>
-      <ProductList />
-    </div>
+    <main className="overflow-x-hidden">
+      <Hero />
+      <Services />
+      <WhereToBuy />
+      <AppDownload />
+      <Footer />
+    </main>
   );
-}
+};
 
 export default App;
